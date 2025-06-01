d1b13452d66dc1d4142983b8301371fd84c4828f
Kim-hyun-hee <asdfzxcv0730@naver.com>
2025-06-01 22:52:12 +0900

    [Utils] MathUtils, RhythmUtils 클래스 생성

---
+++ Assets/Scripts/Utils/MathUtils.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+
+public static class VectorUtils
+{
+    /// <summary>
+    /// angleRadians(라디안)를 기준으로 단위 벡터 반환 (기본적으로 x축 기준 시계 반대 방향)
+    /// </summary>
+    public static Vector3 AngleToVector(float angleRadians)
+    {
+        return new Vector3(Mathf.Cos(angleRadians), Mathf.Sin(angleRadians), 0f);
+    }
+
+    public static Vector3 AngleToVector(double angleRadians)
+    {
+        return new Vector3((float)Math.Cos(angleRadians), (float)Math.Sin(angleRadians), 0f);
+    }
+
+    /// <summary>
+    /// angleRadians(라디안)를 기준으로 시계 방향 단위 벡터 반환
+    /// </summary>
+    public static Vector3 ClockwiseAngleToVector(float angleRadians)
+    {
+        return new Vector3(Mathf.Sin(angleRadians), Mathf.Cos(angleRadians), 0f);
+    }
+
+    public static Vector3 ClockwiseAngleToVector(double angleRadians)
+    {
+        return new Vector3((float)Math.Sin(angleRadians), (float)Math.Cos(angleRadians), 0f);
+    }
+
+    public static double mod(double x, double m)
+    {
+        return (x % m + m) % m;
+    }
+}
---
+++ Assets/Scripts/Utils/RhythmUtils.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class RhythmUtils
+{
+    /// <summary>
+    /// BPM을 crotchet (한 박자, 4분음표)의 길이(초)로 변환
+    /// </summary>
+    public static double BpmToCrotchet(double bpm)
+    {
+        return 60.0 / bpm;
+    }
+
+    /// <summary>
+    /// 경과 시간(초)을 박자 수로 변환
+    /// </summary>
+    public static double TimeToBeats(double time, double bpm)
+    {
+        return time / BpmToCrotchet(bpm);
+    }
+
+    /// <summary>
+    /// 박자 수를 시간(초)으로 변환
+    /// </summary>
+    public static double BeatsToTime(double beats, double bpm)
+    {
+        return beats * BpmToCrotchet(bpm);
+    }
+
+    /// <summary>
+    /// 시간(초)을 각도(라디안)으로 변환. 1박자당 π 라디안 기준
+    /// </summary>
+    public static double TimeToAngleRad(double time, double bpm)
+    {
+        return TimeToBeats(time, bpm) * Math.PI;
+    }
+
+    /// <summary>
+    /// 각도(라디안)를 시간(초)으로 변환. 1박자당 π 라디안 기준
+    /// </summary>
+    public static double AngleRadToTime(double angle, double bpm)
+    {
+        return angle / Math.PI * BpmToCrotchet(bpm);
+    }
+}
